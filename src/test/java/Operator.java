1388aa822909c3917a59b78d068060ec73c47374
kamal belkati <belkam2004@yahoo.fr>
2019-01-24 17:32:13 -0500

    first commit

---
+++ src/test/java/Operator.java
@@ -0,0 +1,16 @@
+public class Operator {
+
+    public static void main(String[] args) {
+
+        int temp =  Add(100, 300);
+        System.out.println(temp);
+
+    }
+
+
+    public static int Add(int x, int y){
+
+        int sum = x + y;
+        return sum;
+    }
+}
